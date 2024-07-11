7a9e09b9162c38d60b527d8b076c2a1d5e95047a
Jeffrey Gong <jeffrey.gong@yale.edu>
2024-07-11 19:55:29 -0400

    wording changes and new is_face_of function

--- filtration-cells/simplicial-complex.py
+++ filtration-cells/simplicial-complex.py
@@ -23,35 +23,38 @@ class VRFiltrationIndexedCell:
         assert(len(set(vertices)) == len(vertices))
         self.vertex_set = set(vertices)
         self.dimension = len(vertices) - 1
-        self.facets: List[Self] = []
-        self.cofacets: List[Self] = []
+        self.co_1_faces: List[Self] = []
+        self.co_neg_1_cofaces: List[Self] = []
     
-    def add_facet(self, other: Self):
+    def add_co_1_face(self, other: Self):
         """
-        Add a facet to the list of facets
+        Add a codimension 1 face
         """
-        self.facets.append(other)
+        assert(other.is_face_of(self) == 1)
+        self.co_1_faces.append(other)
 
-    def add_cofacet(self, other: Self):
-        assert(self.is_facet_of(other))
-        self.cofacets.append(other)
+    def add_co_neg_1_coface(self, other: Self):
+        """
+        Add a codimension -1 face
+        """
+        assert(self.is_face_of(other) == 1)
+        self.co_neg_1_cofaces.append(other)
     
-    def is_facet_of(self, other: Self):
-        # assumes sorted
-        if self.dimension != other.dimension - 1:
-            return False
+    def is_face_of(self, other: Self):
+        """
+        Returns the codimension of other w.r.t self, returns -1 if not a face
+        """
 
-        found_mismatch = False
-
-        for i in range(self.dimension + 1):
-            if found_mismatch:
-                if self.sorted_vertices[i] != other.sorted_vertices[i+1]:
-                    return False 
-            else:
-                if self.sorted_vertices[i] != other.sorted_vertices[i]:
-                    found_mismatch = True 
+        if self.dimension >= other.dimension:
+            return -1
         
-        return True
+        if self.vertex_set.issubset(other.vertex_set):
+            return len(other.vertex_set) - len(self.vertex_set)
+        
+        return -1
+
+
+        
 
     def __repr__(self):
         return f"{self.dimension}-Cell: {self.sorted_vertices}"
@@ -68,6 +71,12 @@ class VRFiltrationSimplicialComplex:
     """
     
     def __init__(self, maximal_simplices: List[VRFiltrationIndexedCell]):
+        self._check_full_vertex_range(maximal_simplices)
+
+
+        #
+    
+    def _check_full_vertex_range(maximal_simplices: List[VRFiltrationIndexedCell]):
         # check that each vertex is represented from [1,n]
         vertex_tracker = set()
         for maximal_simplex in maximal_simplices:
@@ -79,13 +88,11 @@ class VRFiltrationSimplicialComplex:
 
         assert(full_range.issubset(vertex_tracker))
 
-        # 
-
 def main():
     bruh = VRFiltrationIndexedCell([1,2,3])
     bruhh = VRFiltrationIndexedCell([3])
 
-    print(bruhh.is_facet_of(bruh))
+    print(bruhh.is_face_of(bruh))
 
 
 if __name__ == "__main__":
